8e0931ee84f9ec6e025353c988c208fcbfa067f0
shiv.rar <shivan.ram101@gmail.com>
2024-08-06 22:04:05 +0100

    Added singleton access for factory

---
+++ design_patterns/include/Singleton.h
@@ -0,0 +1,32 @@
+#include <memory>
+#include <mutex>
+
+// Singleton class template
+template <typename T, typename... Args>
+class Singleton {
+public:
+    static T& getInstance(Args... args) {
+        std::call_once(initFlag, [&] {
+            instance.reset(new T(std::forward<Args>(args)...));
+        });
+        return *instance;
+    }
+
+    // Delete copy constructor and assignment operator
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+protected:
+    Singleton() = default;
+    virtual ~Singleton() = default;
+
+private:
+    static std::unique_ptr<T> instance;
+    static std::once_flag initFlag;
+};
+
+template <typename T, typename... Args>
+std::unique_ptr<T> Singleton<T, Args...>::instance = nullptr;
+
+template <typename T, typename... Args>
+std::once_flag Singleton<T, Args...>::initFlag;
--- design_patterns/src/main.cpp
+++ design_patterns/src/main.cpp
@@ -1,4 +1,21 @@
 #include "Mediator.h"
+#include "Singleton.h"
+
+// Example class to be managed by the singleton
+class Example
+{
+public:
+    Example(int a, double b) : a_(a), b_(b) {}
+
+    void Display() const
+    {
+        std::cout << "Example instance with a = " << a_ << " and b = " << b_ << std::endl;
+    }
+
+private:
+    int a_;
+    double b_;
+};
 
 int main() {
     ConcreteMediator<std::string> mediator;
@@ -10,6 +27,30 @@ int main() {
 
     compA.send("Hello from A");
     compB.send("Hello from B");
+
+    // Get the singleton instance of std::vector<int>
+    auto& vec = Singleton<std::vector<int>>::getInstance();
+
+    // Add some values to the vector
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    // Print the values in the vector
+    for (const auto& value : vec) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
+    // Ensure that we get the same instance
+    auto& sameVec = Singleton<std::vector<int>>::getInstance();
+    sameVec.push_back(4);
+
+    // Print the values again to confirm it's the same instance
+    for (const auto& value : vec) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
